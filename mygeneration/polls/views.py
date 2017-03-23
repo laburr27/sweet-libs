27e418643700a881e7bdd627f4e0de502c5104e7
Lisa Anne Burr <burr_lisa@bah.com>
2017-03-23 16:43:32 -0400

    added more auto testing functionality; finished Part 5

--- mygeneration/polls/views.py
+++ mygeneration/polls/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.template import loader
+from django.http import HttpResponseRedirect, HttpResponse
 from django.http import Http404
+
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.urls import reverse
+
+from django.template import loader
+
 from django.views import generic
+
+from django.urls import reverse
+
+from django.utils import timezone
 
 from .models import Question
 
@@ -17,14 +23,23 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions(not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
